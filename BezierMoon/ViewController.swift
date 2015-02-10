6d1e2427c68e18baaee0e8e646d3fa43435892d5
riconroy <riconroy@gmail.com>
2015-02-09 19:31:24 -0500

    Added to tap gesture to control animation; animates half moon in and out; uses a "fake" rectangle since the path animation needs similar control points to work (try taking out the add quad curve for whacky animation!).

--- BezierMoon/ViewController.swift
+++ BezierMoon/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 	
 	var myImageView: UIImageView!
+	var isMoonVisible: Bool!
+	var myBezierMoon: UIBezierPath!
+	var myBezierTrivial: UIBezierPath!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -26,20 +29,58 @@ class ViewController: UIViewController {
 		myImageView.frame = imageFrame
 		myImageView.center = self.view.center
 		
-		// add the bezier path
-		let myBezierPath = bezierCurveWithHalfMoonOnLeft(myImageView.frame)
-		var shapelayer = CAShapeLayer()
-		shapelayer.path = myBezierPath.CGPath
-		myImageView.layer.mask = shapelayer
+		// create two bezier paths, one trivial
+		myBezierMoon = bezierCurveWithHalfMoonOnLeft(myImageView.frame)
+		myBezierTrivial = bezierCurveTrivial(myImageView.frame)
 		
-		// add
+		// start with no bite taken out
+		var shapelayerOne = CAShapeLayer()
+		shapelayerOne.path = myBezierTrivial.CGPath
+		myImageView.layer.mask = shapelayerOne
+		
+		// set boolean
+		isMoonVisible = false
+		
+		// add to view (and make sure user interaction enabled!)
+		myImageView.userInteractionEnabled = true
 		self.view.addSubview(myImageView)
+		
+		// let the image recognize a tap
+		let tapGesture = UITapGestureRecognizer(target: self, action: "myTap:")
+		myImageView.addGestureRecognizer(tapGesture)
+	}
+	
+	// when the user taps on the image
+	func myTap(sender: UITapGestureRecognizer) {
+		// println("user tapped me")
+		toggleBezierMoon()
+	}
+	
+	func toggleBezierMoon () {
+		let myAnimation = CABasicAnimation(keyPath: "path")
+		
+		if (isMoonVisible == true) {
+			myAnimation.fromValue = myBezierMoon.CGPath
+			myAnimation.toValue = myBezierTrivial.CGPath
+		} else {
+			myAnimation.fromValue = myBezierTrivial.CGPath
+			myAnimation.toValue = myBezierMoon.CGPath
+		}
+		
+		myAnimation.duration = 0.4
+		myAnimation.fillMode = kCAFillModeForwards
+		myAnimation.removedOnCompletion = false
+		
+		myImageView.layer.mask.addAnimation(myAnimation, forKey: "animatePath")
+		
+		// reverse the boolean
+		isMoonVisible = !isMoonVisible
 	}
 	
 	func bezierCurveWithHalfMoonOnLeft(rect: CGRect) -> UIBezierPath {
 		let initialPoint = CGPoint(x: 0, y: 0)
 		let curveStart = CGPoint(x: 0, y: (rect.size.height) * (0.2))
-		let curveControl = CGPoint(x: (rect.size.width) * (0.4), y: (rect.size.height) * (0.5))
+		let curveControl = CGPoint(x: (rect.size.width) * (0.6), y: (rect.size.height) * (0.5))
 		let curveEnd = CGPoint(x: 0, y: (rect.size.height) * (0.8))
 		let firstCorner = CGPoint(x: 0, y: rect.size.height)
 		let secondCorner = CGPoint(x: rect.size.width, y: rect.size.height)
@@ -53,6 +94,27 @@ class ViewController: UIViewController {
 //		let firstCorner = CGPoint(x: rect.origin.x, y: rect.origin.y + rect.size.height)
 //		let secondCorner = CGPoint(x: rect.origin.x + rect.size.width, y: rect.origin.y + rect.size.height)
 //		let thirdCorner = CGPoint(x: rect.origin.x + rect.size.width, y: rect.origin.y)
+		
+		var myBezier = UIBezierPath()
+		myBezier.moveToPoint(initialPoint)
+		myBezier.addLineToPoint(curveStart)
+		myBezier.addQuadCurveToPoint(curveEnd, controlPoint: curveControl)
+		myBezier.addLineToPoint(firstCorner)
+		myBezier.addLineToPoint(secondCorner)
+		myBezier.addLineToPoint(thirdCorner)
+		
+		myBezier.closePath()
+		return myBezier
+	}
+	
+	func bezierCurveTrivial(rect: CGRect) -> UIBezierPath {
+		let initialPoint = CGPoint(x: 0, y: 0)
+		let curveStart = CGPoint(x: 0, y: (rect.size.height) * (0.2))
+		let curveControl = CGPoint(x: 0, y: (rect.size.height) * (0.5))
+		let curveEnd = CGPoint(x: 0, y: (rect.size.height) * (0.8))
+		let firstCorner = CGPoint(x: 0, y: rect.size.height)
+		let secondCorner = CGPoint(x: rect.size.width, y: rect.size.height)
+		let thirdCorner = CGPoint(x: rect.size.width, y: 0)
 		
 		var myBezier = UIBezierPath()
 		myBezier.moveToPoint(initialPoint)
